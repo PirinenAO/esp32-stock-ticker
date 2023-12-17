d65206e533f8e3b64d463d5158335d9cdd50e088
api <e2202869@edu.vamk.fi>
2023-12-17 21:12:03 +0200

    init commit

--- include/wifi_credentials.h
+++ include/wifi_credentials.h
@@ -1,7 +1,7 @@
 #ifndef WIFI_CREDENTIALS_H
 #define WIFI_CREDENTIALS_H
 
-const char *ssid = "WIFI-AP";
-const char *passwd = "aJ7Q/T10)Dp1-t0";
+const char *ssid = "your-ssid";
+const char *passwd = "your-password";
 
 #endif // WIFI_CREDENTIALS_H
